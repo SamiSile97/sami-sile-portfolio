b0b5d996107932ed941e605e2ef82f5f0abb14ef
samisile <samisile97@gmail.com>
2023-08-03 16:58:10 +0100

    added image, font and did some basic CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Portfolio comin sooooonnnnnn
+    <div className="name-and-image">
+        <div className='name-and-title'>
+          <h1>Sami Sile</h1>
+          <div className='line'></div>
+          <h2>Junior Full-Stack Developer</h2>
+        </div>
+        <div>
+          <img src="headshot.JPG" alt="headshot of sami sile smiling"></img>
+        </div>
+      </div>
     </div>
   );
 }
