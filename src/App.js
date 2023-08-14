2281cae57bafb06b5991f633b8edf0fc9dbf2418
samisile <samisile97@gmail.com>
2023-08-14 11:49:05 +0100

    added a video background

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@ import { projects } from './Data/projects';
 
 function App() {
   return (
-    <div className="App">
-    <div className="name-and-image">
-        <div className='name-and-title'>
-          <h1>Sami Sile</h1>
-          <div className='line'></div>
-          <h2>Junior Full-Stack Developer</h2>
+    <div className='bg-test'>
+      <div className="App">
+        <video src="ray.mp4" autoPlay loop muted></video>
+      <div className="name-and-image">
+          <div className='name-and-title'>
+            <h1>Sami Sile</h1>
+            <div className='line'></div>
+            <h2>Junior Full-Stack Developer</h2>
+          </div>
+          <div>
+            <img src="headshot.JPG" className='headshot' alt="headshot of sami sile smiling"></img>
+          </div>
         </div>
-        <div>
-          <img src="headshot.JPG" className='headshot' alt="headshot of sami sile smiling"></img>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="connect">
+          <h3 className="connect-sentence"><span className="connect-word">Connect</span> with me...</h3>
+          <a href='https://www.linkedin.com/in/sami-sile/' target='_blank' rel="noreferrer"><img src="linkedin.svg" className="linkedin" alt='linkedin logo'></img></a>
+          <a href='https://github.com/SamiSile97' target='_blank' rel="noreferrer"><img src="github.svg" className="github" alt='github logo'></img></a>
+          <a href='mailto:samisile97@gmail.com' target='_blank' rel="noreferrer"><img src="email.svg" className="email" alt='email graphic'></img></a>
         </div>
-      </div>
-      <div className="circle1"></div>
-      <div className="circle2"></div>
-      <div className="connect">
-        <h3 className="connect-sentence"><span className="connect-word">Connect</span> with me</h3>
-        <a href='https://www.linkedin.com/in/sami-sile/' target='_blank' rel="noreferrer"><img src="linkedin.svg" className="linkedin" alt='linkedin logo'></img></a>
-        <a href='https://github.com/SamiSile97' target='_blank' rel="noreferrer"><img src="github.svg" className="github" alt='github logo'></img></a>
-        <a href='mailto:samisile97@gmail.com' target='_blank' rel="noreferrer"><img src="email.svg" className="email" alt='email graphic'></img></a>
-      </div>
-      <div className='projects'>
-        {projects.map((data, index) => {return <ProjectCard img={data.img} text={data.text} link={data.link} key={index} />})}
+        <div className='projects-section'>
+          <h1 className='projects-section-h2'>Projects</h1>
+          <div className='projects'>
+            {projects.map((data, index) => {return <ProjectCard img={data.img} text={data.text} link={data.link} key={index} />})}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Data/projects.js
+++ src/Data/projects.js
@@ -2,12 +2,13 @@
 export const projects = [
     {
         img : "walkmate.png",
-        text : "a wonderful walking app",
+        text : "A walking route finder and planner, with the additional benefit of community walks to help you find and connect with the people around you through nature.",
         link : "https://walkmate.netlify.app/",
     },
     {
         img : "codemaster.png",
-        text : "a wonderful coding app"
+        text : "A study buddy app that contains a quiz for testing your coding knowledge, as well as flashcards and resources for further learning.",
+        link : "https://github.com/SchoolOfCode/bc14_w7_project-frontend-go-gitters"
     },
     // {
     //     img : "nathans landscaping.JPG",
